b0ccb334d769d44ab8ed5fc904c053409adce201
sudeepbhandari6545 <sudeepbhandari6545@gmail.com>
2021-05-10 19:17:50 +0545

    disnry-clone

---
+++ src/components/Header.jsx
@@ -0,0 +1,23 @@
+import React from 'react';
+import styled from 'styled-components';
+
+const Header = () => {
+  return (
+    <Nav>
+      <Logo src="" />
+      <NavMenu></NavMenu>
+    </Nav>
+  );
+};
+
+export default Header;
+
+const Nav = styled.nav`
+  height: 70px;
+  background: #090b13;
+`;
+
+const Logo = styled.img`
+  width: 70px;
+`;
+const NavMenu = styled.div``;
