d39f3bbb066119e5a5d4a59548ef8ba6863ff963
sudeepbhandari6545 <sudeepbhandari6545@gmail.com>
2021-05-15 21:10:26 +0545

    jjj

--- src/App.js
+++ src/App.js
@@ -2,14 +2,25 @@ import React from 'react'
 import { Counter } from './features/counter/Counter'
 import Header from './components/Header'
 import Home from './components/Home'
+import { BrowserRouter as Router, Route, Switch, link } from 'react-router-dom'
+import Detail from './components/Detail'
 
 import './App.css'
 
 function App() {
   return (
     <div className="App">
-      <Header />
-      <Home />
+      <Router>
+        <Header />
+        <Switch>
+          <Route path="detail">
+            <Detail />
+          </Route>
+          <Route path="/">
+            <Home />
+          </Route>
+        </Switch>
+      </Router>
     </div>
   )
 }
